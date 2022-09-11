91679297e4edc36d020b84e08f2e0664a9976448
Rohit Sinha <rohitsinha952@gmail.com>
2022-09-11 22:43:28 +0530

    Create urls.tsx

---
+++ NTTDataAssignment/src/config/urls.tsx
@@ -0,0 +1,4 @@
+export const URLS = {
+  coinList: 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&sparkline=false',
+  coinDetails: 'https://api.coingecko.com/api/v3/coins',
+}
