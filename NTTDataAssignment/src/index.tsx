a849bd4ef787947361f4cc20d2e3e87335d8c135
Rohit Sinha <rohitsinha952@gmail.com>
2022-09-11 22:42:13 +0530

    Create index.tsx

---
+++ NTTDataAssignment/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TableComponent from './component/table';
+import './style.css';
+
+ReactDOM.render(<TableComponent />, document.getElementById('root'))
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
